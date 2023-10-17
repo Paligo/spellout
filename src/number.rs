34b384714ae770db3246617a009b8d8610eeffe0
Martijn Faassen <faassen@startifact.com>
2023-10-17 12:38:31 +0200

    Start to flesh out the details.

--- src/number.rs
+++ src/number.rs
@@ -1,25 +1,50 @@
+use core::num;
+
 use icu_locid::{locale, Locale};
+
+#[derive(Debug)]
+enum Error {
+    UnsupportedLocale,
+    NumberOutOfRange,
+}
 
 enum NumberType {
     Cardinal,
+    Ordinal,
 }
 
-struct Modifier {}
+struct NumberModifier {
+    number_type: NumberType,
+}
 
-impl Modifier {
+impl NumberModifier {
     fn new(number_type: NumberType, format: &str) -> Self {
-        Modifier {}
+        NumberModifier { number_type }
     }
 }
-struct Spellout {}
 
-fn spellout(locale: Locale) -> Spellout {
-    Spellout {}
+fn spellout_number(
+    locale: Locale,
+    modifier: NumberModifier,
+) -> Result<impl Fn(u64) -> Result<String, Error>, Error> {
+    match modifier.number_type {
+        NumberType::Cardinal => Ok(spellout_en_cardinal as fn(u64) -> Result<String, Error>),
+        NumberType::Ordinal => Ok(spellout_en_ordinal as fn(u64) -> Result<String, Error>),
+    }
 }
 
-impl Spellout {
-    fn number(&self, num: u64, modifier: Modifier) -> String {
-        "three".to_string()
+fn spellout_en_cardinal(num: u64) -> Result<String, Error> {
+    match num {
+        2 => Ok("two".to_string()),
+        3 => Ok("three".to_string()),
+        _ => Err(Error::NumberOutOfRange),
+    }
+}
+
+fn spellout_en_ordinal(num: u64) -> Result<String, Error> {
+    match num {
+        2 => Ok("second".to_string()),
+        _ => Err(Error::NumberOutOfRange),
     }
 }
 
@@ -28,10 +53,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_spellout_number() {
-        let spellout_en = spellout(locale!("en"));
-        let modifier = Modifier::new(NumberType::Cardinal, "stuff");
-        assert_eq!(spellout_en.number(3, modifier), "three");
-        // assert_eq!(spellout_number(3, modifier, locale!("en")), "three");
+    fn test_spellout_number_three() {
+        let modifier = NumberModifier::new(NumberType::Cardinal, "stuff");
+        let spellout_number_en_cardinal = spellout_number(locale!("en"), modifier).unwrap();
+        assert_eq!(spellout_number_en_cardinal(3).unwrap(), "three");
+    }
+
+    #[test]
+    fn test_spellout_number_two() {
+        let modifier = NumberModifier::new(NumberType::Cardinal, "stuff");
+        let spellout_number_en_cardinal = spellout_number(locale!("en"), modifier).unwrap();
+        assert_eq!(spellout_number_en_cardinal(2).unwrap(), "two");
+    }
+    #[test]
+    fn test_spellout_number_two_ordinal() {
+        let modifier = NumberModifier::new(NumberType::Ordinal, "stuff");
+        let spellout_number_en_cardinal = spellout_number(locale!("en"), modifier).unwrap();
+        assert_eq!(spellout_number_en_cardinal(2).unwrap(), "second");
     }
 }
